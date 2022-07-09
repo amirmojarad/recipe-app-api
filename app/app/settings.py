c3174217a7e85bd31d90330b6ce86e8fed2c8de4
amirmojarad <amirmojarad13@gmail.com>
2022-07-09 16:34:58 +0430

    wait_for_db and database race condition solved

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # recipe-api apps
+    'core',
 ]
 
 MIDDLEWARE = [
